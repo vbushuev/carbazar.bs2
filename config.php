99887a2823a6e682647a530a4bae8f76d73c4911
vbushuev <v.bushuev@gmail.com>
2017-04-28 00:52:06 +0300

    kb

--- config.php
+++ config.php
@@ -2,6 +2,13 @@
 $cbConfig = [
     "clientBase" => [
         "key" => "615cdb1dd19ef65bcb7c81a97360abc0",
+        "host" =>"http://fb79056g.beget.tech/api",
+        "version" =>"2.0",
+        "login" => "admin",
+        "reportTable" => "280"
+    ],
+    "clientBaseLocal" => [
+        "key" => "615cdb1dd19ef65bcb7c81a97360abc0",
         "host" =>"http://cb.bs2/api",
         "version" =>"1.0",
         "login" => "admin",
